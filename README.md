d6f2375e3e982e773c854237957c1aaf73e09e4e
ramen72 <ramen@devsramen.com>
2025-05-11 09:34:45 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OYA-Project-16-E-Shop
+This is a E-commerce project.
---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from 'react-router-dom'
+import './assets/css/tailwind.css'
+import HomePage from './pages/HomePage';
+import ProductListPages from './pages/ProductListPages';
+import ProductDetailsPages from './pages/ProductDetailsPages';
+import CartPage from './pages/CartPage';
+import CheckOutPage from './pages/CheckOutPage';
+import RegisterPage from './pages/RegisterPage';
+import MyAccountsPage from './pages/MyAccountsPage';
+import BlogsPage from './pages/BlogsPage';
+import BlogDetailsPage from './pages/BlogDetailsPage';
+import ContactPage from './pages/ContactPage';
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route index element={<HomePage/>}/>
+      <Route path='/product-list' element={<ProductListPages/>}/>
+      <Route path='/product-details' element={<ProductDetailsPages/>}/>
+      <Route path='/cart' element={<CartPage/>}/>
+      <Route path='/checkout' element={<CheckOutPage/>}/>
+      <Route path='/register' element={<RegisterPage/>}/>
+      <Route path='/my-account' element={<MyAccountsPage/>}/>
+      <Route path='/blogs' element={<BlogsPage/>}/>
+      <Route path='/blog-details' element={<BlogDetailsPage/>}/>
+      <Route path='/contact' element={<ContactPage/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
