ecc8f5869d35f5dc7138674809841338e4d3b0be
ramen72 <ramen@devsramen.com>
2025-07-06 17:23:40 +0600

    Update Cart page at 06-07-2025_1723

---
+++ src/data/CartData.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const CartData = [
+  {
+    imageUrl: "../../public/images/productImage-3.jpg",
+    category: "LAPTOP",
+    title: "2019 Smart Laptop 256 GB 13 inch Pro Chip Core 1 TB HD SSD",
+    price: 1659,
+    qty: 1,
+    total: 1659,
+    Variant: "Space Gray",
+  },
+  {
+    imageUrl: "../../public/images/productImage-1.png",
+    category: "watch",
+    title: "Mobile Watch A Series GPS 7/144 45 mm Red Rubber Band",
+    price: 999,
+    qty: 1,
+    total: 999,
+    Variant: "Fiery Red",
+  },
+  {
+    imageUrl: "../../public/images/productImage-5.jpg",
+    category: "Television",
+    title: "LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color USB HQ",
+    price: 2299,
+    qty: 1,
+    total: 2299,
+    Variant: "Jet Black",
+  },
+  {
+    imageUrl: "../../public/images/productImage-2.jpg",
+    category: "gaming",
+    title: "Console Game Stick HD Wireless Bluetooth Connect",
+    price: 109,
+    qty: 1,
+    total: 109,
+    Variant: "White",
+  },
+  {
+    imageUrl: "../../public/images/productImage-6.jpg",
+    category: "Camera",
+    title: "20 MP Canon 1500d DSLR Camera",
+    price: 259,
+    qty: 1,
+    total: 259,
+    Variant: "Black (with Bag)",
+  },
+];
+
+export default CartData;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/commonLayouts/Container";
+import CartData from "../data/CartData";
+import { Link } from "react-router-dom";
+import Button from "./../components/Button";
 
 const CartPage = () => {
+  const [cartData, setCartData] = useState(CartData);
+  const [totalAmount, setTotalAmount] = useState(5325);
+
+  const handleClickForQty = (index, type) => {
+    const updatedCart = [...cartData];
+
+    if (type === "Plus") {
+      updatedCart[index].qty += 1;
+    } else if (type === "Minus" && updatedCart[index].qty > 1) {
+      updatedCart[index].qty -= 1;
+    }
+
+    updatedCart[index].total =
+      updatedCart[index].qty * updatedCart[index].price;
+    setCartData(updatedCart);
+
+    const totalAmount = updatedCart.reduce((acc, item) => acc + item.total, 0);
+    setTotalAmount(totalAmount);
+  };
+
   return (
     <>
       <Container>
@@ -21,6 +44,136 @@ const CartPage = () => {
           >
             Your Cart
           </h1>
+          <div>
+            <div
+              className={`bg-lightGray py-8 px-14 rounded-[15px] flex font-["montserrat"] font-bold text-base text-black`}
+            >
+              <div className={`w-[49.1477%] uppercase`}>
+                <h3>PRODUCT</h3>
+              </div>
+              <div className={`w-[15.8381%] uppercase`}>
+                <h3>PRICE</h3>
+              </div>
+              <div className={`w-[16.8324%] uppercase`}>
+                <h3>QTY</h3>
+              </div>
+              <div className={`w-[18.1818%] uppercase`}>
+                <h3>TOTAL</h3>
+              </div>
+            </div>
+            <ul>
+              {cartData.map((item, index) => (
+                <li
+                  key={index}
+                  className={`p-10 first:pt-8 rounded-[15px] flex items-center font-["montserrat"] font-bold text-base text-black border border-transparent hover:border-black-25`}
+                >
+                  <div
+                    className={`w-[49.4444%] pr-[90px] flex justify-between items-center gap-x-[62px]`}
+                  >
+                    <div className={`w-[44.55%] h-[214px]`}>
+                      <img src={item.imageUrl} alt="" />
+                    </div>
+                    <div className={`w-[58.0056%]`}>
+                      <p
+                        className={`font-["montserrat"] font-normal text-base text-black tracking-[5px] uppercase mb-4`}
+                      >
+                        {item.category}
+                      </p>
+                      <h3
+                        className={`font-["poppins"] font-semibold text-xl text-black`}
+                      >
+                        {item.title}
+                      </h3>
+                    </div>
+                  </div>
+                  <div className={`w-[15.8381%] uppercase`}>
+                    <h3
+                      className={`font-["poppins"] font-semibold text-xl text-black`}
+                    >
+                      <span>$</span>
+                      {item.price}
+                    </h3>
+                  </div>
+                  <div
+                    className={`w-[16.8324%] flex justify-start items-center gap-x-4`}
+                  >
+                    <button
+                      onClick={() => handleClickForQty(index, "Minus")}
+                      className={`w-10 h-10 font-bold text-4xl rounded-full flex justify-center items-center hover:bg-lightGray cursor-pointer`}
+                    >
+                      -
+                    </button>
+                    <span
+                      className={`w-10 h-10 bg-lightGray rounded-full flex justify-center items-center`}
+                    >
+                      {item.qty}
+                    </span>
+                    <button
+                      onClick={() => handleClickForQty(index, "Plus")}
+                      className={`w-10 h-10 font-bold text-2xl rounded-full flex justify-center items-center hover:bg-lightGray cursor-pointer`}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div className={`w-[18.1818%]`}>
+                    <h3
+                      className={`font-["poppins"] font-semibold text-xl text-black`}
+                    >
+                      <span>$</span>
+                      {item.total}
+                    </h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className={`flex justify-between items-center mt-8`}>
+              <div
+                className={`w-[40.6579%] border border-black-25 rounded-[10px] py-8 px-9 flex justify-between items-center`}
+              >
+                <input
+                  type="text"
+                  className={`w-3/4 placeholder:font-["montserrat"] text-xl outline-0 placeholder:font-normal placeholder:text-base placeholder:text-black-75`}
+                  placeholder="Enter coupon code (ex: FIRSTPAY)"
+                />
+                <Link
+                  to={"#"}
+                  className={`font-["poppins"] font-semibold text-xl text-black capitalize`}
+                >
+                  Apply Code
+                </Link>
+              </div>
+              <div
+                className={`w-[40.6579%] border border-black-25 rounded-[10px] py-8 px-9 flex justify-between items-center`}
+              >
+                <h4
+                  className={`font-["montserrat"] font-bold text-base text-black uppercase`}
+                >
+                  SUB TOTAL
+                </h4>
+                <p
+                  className={`font-["poppins"] font-semibold text-2xl text-orange flex justify-end items-baseline`}
+                >
+                  <span>$</span>
+                  {totalAmount}
+                  <p
+                    className={`font-["montserrat"] font-normal text-sm text-black pl-6 `}
+                  >
+                    ( excl. shipping fee )
+                  </p>
+                </p>
+              </div>
+            </div>
+            <div className={`flex justify-end items-center gap-x-14 mt-8`}>
+              <h3
+                className={`font-["poppins"] font-semibold text-xl text-black capitalize`}
+              >
+                Continue Shopping
+              </h3>
+              <Link to={"/product-list"}>
+                <Button text="Update Cart" />
+              </Link>
+            </div>
+          </div>
         </div>
       </Container>
     </>
