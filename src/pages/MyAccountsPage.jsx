fc887a5d345606c89560d9b54ec9401e5f3c61cf
ramen72 <ramen@devsramen.com>
2025-07-07 20:08:05 +0600

    Update MyAccountsPage.jsx

--- src/pages/MyAccountsPage.jsx
+++ src/pages/MyAccountsPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import Container from "../components/commonLayouts/Container";
 
 const MyAccountsPage = () => {
-    return (
-        <div>
-            MyAccountsPage
-        </div>
-    );
+  return (
+    <>
+      <div className={`mt-16 mb-[122px]`}>
+        <Container>
+          <div className={`flex justify-between items-center`}>
+            <div className={`w-1/4 h-[760px] border rounded-[25px]`}>text</div>
+            <div className={`w-[71.8421%] h-[760px] border rounded-[25px]`}>
+              text
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default MyAccountsPage;
