dc74482931dffdc9a37b2baa22625648dd0945b2
Vercel <vercel@devsramen.com>
2025-07-09 12:23:15 +0600

    Update Blog Details page at 09-07-2025_1223

---
+++ src/data/BlogDetailsData.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const BlogDetailsData = [
+  {
+    title: "5G Connectivity",
+    details:
+      "5G connectivity is the latest generation of mobile network technology, and it promises to revolutionize the way we use our devices. With faster speeds and lower latency, 5G connectivity will enable new applications such as remote surgery, augmented reality, and autonomous vehicles. ",
+  },
+  {
+    title: "Artificial Intelligence (AI)",
+    details:
+      "AI is transforming the way we interact with our devices and the world around us. From virtual assistants like Siri and Alexa to self-driving cars and smart homes, AI is changing the way we live and work. ",
+  },
+  {
+    title: "Virtual Reality (VR) and Augmented Reality (AR)",
+    details:
+      "VR and AR are technologies that enable us to experience digital content in a more immersive way. From gaming and entertainment to education and training, these technologies have the potential to transform the way we learn and experience the world. ",
+  },
+  {
+    title: "Internet of Things (IoT)",
+    details:
+      "The IoT is a network of devices that are connected to the internet, allowing them to communicate and exchange data. This technology has the potential to transform many industries, from healthcare and agriculture to transportation and manufacturing. ",
+  },
+  {
+    title: "Edge Computing",
+    details:
+      "Edge computing is a new computing paradigm that brings computing power closer to the devices that generate and consume data. By processing data at the edge of the network, edge computing can reduce latency, improve security, and enhance privacy. ",
+  },
+  {
+    title: "Blockchain",
+    details:
+      "Blockchain is a technology that enables secure, decentralized transactions without the need for a central authority. From cryptocurrency to supply chain management, blockchain has the potential to transform many industries. ",
+  },
+  {
+    title: "Quantum Computing",
+    details:
+      "Quantum computing is a new type of computing that uses the principles of quantum mechanics to perform calculations. With the potential to solve complex problems that are beyond the capabilities of classical computers, quantum computing has the potential to revolutionize many industries, from finance and healthcare to energy and transportation. ",
+  },
+];
+
+export default BlogDetailsData;
--- src/pages/BlogDetailsPage.jsx
+++ src/pages/BlogDetailsPage.jsx
@@ -3,6 +3,7 @@ import Container from "../components/commonLayouts/Container";
 import BlogLeftSideBar from "../components/BlogLeftSideBar";
 import { HiMiniClock, HiUser } from "react-icons/hi2";
 import { FaComment } from "react-icons/fa";
+import BlogDetailsData from "../data/BlogDetailsData";
 
 const BlogDetailsPage = () => {
   return (
@@ -30,7 +31,7 @@ const BlogDetailsPage = () => {
             <div className={`w-[28.8816%]`}>
               <BlogLeftSideBar />
             </div>
-            <div className={`w-[67.5%] border`}>
+            <div className={`w-[67.5%]`}>
               <h1
                 className={`w-[85%] font-["poppins"] font-bold text-[56px] leading-[68px] capitalize`}
               >
@@ -55,6 +56,127 @@ const BlogDetailsPage = () => {
                   <FaComment className={`text-3xl text-black-75`} />
                   432 comments
                 </p>
+              </div>
+              <div
+                className={`w-full h-[645px] bg-lightGray rounded-[25px] my-12`}
+              >
+                <img
+                  src="../../public/images/blogImage-3.jpg"
+                  alt="BlogImage"
+                  className="w-full h-full rounded-[25px] object-cover"
+                />
+              </div>
+              <p
+                className={`font-["montserrat"] font-normal text-xl text-black leading-[30px] mb-8`}
+              >
+                Technology is constantly evolving, and keeping up with the
+                latest trends can be a daunting task. From new gadgets and
+                devices to emerging technologies and industry shifts, it can be
+                challenging to stay up to date with the latest tech trends. In
+                this article, we'll explore some of the latest tech trends that
+                you need to know about to stay ahead of the curve.
+              </p>
+              <ul className={`flex flex-col gap-y-6`}>
+                {BlogDetailsData.map((item, index) => (
+                  <li className={`flex justify-start items-start gap-x-[30px]`}>
+                    <div className={``}>
+                      <span className='bg-orange w-[64px] h-[64px] font-["Montserrat"] font-semibold text-2xl text-white rounded-full text-center flex justify-center items-center'>
+                        {index + 1}
+                      </span>
+                    </div>
+                    <div>
+                      <h4
+                        className={`font-["montserrat"] font-bold text-xl text-black leading-[30px]`}
+                      >
+                        {item.title}
+                      </h4>
+                      <p
+                        className={`font-["montserrat"] font-normal text-xl text-black leading-[30px] mt-2`}
+                      >
+                        {item.details}
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <p
+                className={`font-["montserrat"] font-normal text-xl text-black leading-[30px] mt-8`}
+              >
+                In conclusion, staying up to date with the latest tech trends is
+                essential for anyone who wants to stay ahead of the curve. From
+                5G connectivity and AI to VR and AR, these technologies have the
+                potential to transform the way we live and work. By keeping up
+                with the latest trends and innovations, you can prepare yourself
+                for the future and stay ahead of the competition.
+              </p>
+              <div
+                className={`mt-8 py-7 px-8 bg-lightGray flex flex-wrap justify-start items-start gap-2`}
+              >
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Technology
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Trends
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Innovation
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Tips
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Artificial Intelligence
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Virtual Reality
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Augmented Reality
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Internet of Things
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Internet of Things
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Blockchain
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Quantum Computing
+                </span>
+                <span
+                  className={`inline-block py-3 px-[20px] bg-white rounded-[5px]`}
+                >
+                  Emerging Technologies
+                </span>
+              </div>
+              <div className={`mt-20 flex justify-between items-center`}>
+                <div>Prev post</div>
+                <div>Social media</div>
+                <div>Next post</div>
               </div>
             </div>
           </div>
